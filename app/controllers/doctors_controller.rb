eac41196620ccd1ed00f95531df0279ea4b743a7
Charles Buxton <chazbux@gmail.com>
2020-02-24 15:20:52 -0700

    added appointments controller

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,13 +1,52 @@
 class DoctorsController < ApplicationController
-  def index
+  before_action :set_doctor, only: [:show, :edit, :update, :destroy]
+  #set_*model* is private method that loads in called index automatically
+ def index
+    @doctors = Doctor.all
   end
-
-  def show
+  
+ def new
+    @doctor = Doctor.new
   end
-
-  def new
+  
+ def create
+    @doctor = Doctor.new(doctor_params)
+    if @doctor.save
+      redirect_to doctors_path
+    else
+      render :new
+    end
   end
-
-  def edit
+  
+ def show
+   #before_action
+ end
+  
+ def edit
+   #before_action
+ end
+  
+ def update
+    @doctor = Doctor.find(params[:id])
+    if @doctor.update(doctor_params)
+      redirect_to doctors_path
+    else
+      render :edit
+    end
   end
-end
+  
+ def destroy
+    @doctor.destroy
+    redirect_to doctors_path
+  end
+  
+ private
+  def doctor_params
+    params.require(:doctor).permit(:first_name, :last_name, :specialty)
+  end
+  
+ def set_doctor
+    @doctor = Doctor.find(params[:id])
+  end
+ end
+ 
